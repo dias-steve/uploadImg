edd7309a3a8a200359db78c0825b56a43bb4ee71
Steve Dias <stevediaspro@gmail.com>
2023-01-06 09:15:44 +0100

    [CI] done

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,8 +1,49 @@
+const path = require('path')
 const {StatusCodes} = require('http-status-codes');
+const CustomError = require('../errors');
+const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
+
+const uploadProductImageLocal = async (req, res) => {
+
+    console.log(req.files);
+    if(!req.files){
+        throw new CustomError.BadRequestError('No File Uploaded');
+    }
+    const productImage =req.files.image;
+    if(!productImage.mimetype.startsWith('image')){
+        throw new CustomError.BadRequestError('Please Upload Image');
+    }
+
+    const maxSize = 1024 * 1024 *1024;
+
+    if(productImage.size > maxSize){
+        throw new CustomError.BadRequestError('Please upload Image smaller');
+    }
+
+    const imagePath = path.join(__dirname, '../public/uploads/'+`${productImage.name}`);
+    console.log(imagePath)
+    await productImage.mv(imagePath);
+
+
+    res.status(StatusCodes.OK).json({image:{src:`/uploads/${productImage.name}`}});
+}
 
 const uploadProductImage = async (req, res) => {
-    console.log(req)
-    res.send('upload');
+  
+    //We send image file to cloudinary server
+        const result = await cloudinary.uploader.upload(
+            req.files.image.tempFilePath,
+            {
+            use_filename: true,
+            folder: 'file-upload'
+    
+        });//we adding tempFilePath white de fileUpload middleware
+
+    //we clean the tmp folder when we have finished to upload the image to cloudinary
+    fs.unlinkSync(req.files.image.tempFilePath)
+    return res.status(StatusCodes.OK).json({image:{src:result.secure_url}});
 }
+
 
 module.exports = {uploadProductImage}
